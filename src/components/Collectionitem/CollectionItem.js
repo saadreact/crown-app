dddd9bd046959ad6fff8f0d3351f1a3d95eecd1a
Muhammadalikhalid <alikhalidramay@gmail.com>
2022-06-09 18:05:13 +0500

    adjusted data for shop page and made changes for cart with new quantity key

--- src/components/Collectionitem/CollectionItem.js
+++ src/components/Collectionitem/CollectionItem.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { addCartItems } from '../../redux/slices/cartSlice';
+import CustomButton from '../customButton/CustomButton';
 import './CollectionItem.scss';
 
+
 const CollectionItem = ({item}) => {
+  const dispatch = useDispatch();
   return (
     <div className="collection-item">
         <div className="image" style={{backgroundImage:`url(${item.imageUrl})`}}></div>
@@ -9,6 +14,7 @@ const CollectionItem = ({item}) => {
             <span className="name">{item.name}</span>
             <span className="price">{item.price}</span>
         </div>
+        <CustomButton inverted onClick={() => dispatch(addCartItems(item))}>Add to cart</CustomButton>
     </div>
   )
 }
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -1,18 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { addItemsWithQuantityCheck } from '../cartUtils/cartutil';
 
 export const cartSlice = createSlice({
   name: 'cart',
   initialState: {
-    hidden: false
+    hidden: false,
+    cartItems:[]
   },
   reducers: {
     toggleCart: (state, action) => {
       state.hidden = !state.hidden
+    },
+    addCartItems: (state, action) =>{
+        state.cartItems = addItemsWithQuantityCheck(state.cartItems,action.payload)
     }
   }
 })
 
 // Action creators are generated for each case reducer function
-export const { toggleCart } = cartSlice.actions
+export const { toggleCart, addCartItems } = cartSlice.actions
 
 export const cartReducer =  cartSlice.reducer
