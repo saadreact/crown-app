2393e6474c7fa88c969e8cf569d08cfbb13f886c
syed <saadshah201@gmail.com>
2022-06-22 01:34:08 +0500

    logger code condition modified

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { persistedReducer } from "./rootReducer";
-import { persistStore } from 'redux-persist';
+import { persistStore } from "redux-persist";
 
-export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: false,
-    }).concat(process.env.NODE_ENV == "development" ? logger : null)
-  });
+const config =
+  process.env.NODE_ENV == "development"
+    ? {
+        reducer: persistedReducer,
+        middleware: (getDefaultMiddleware) =>
+          getDefaultMiddleware({
+            serializableCheck: false,
+          }).concat(logger)
+      }
+    : {
+        reducer: persistedReducer,
+        middleware: (getDefaultMiddleware) =>
+          getDefaultMiddleware({
+            serializableCheck: false,
+          })
+      };
 
-  export const persistor = persistStore(store);
+export const store = configureStore(config);
+
+export const persistor = persistStore(store);
